d974f5066050b5a78574d00ba351e1f8d2786767
Braulio Mesquita <brauliomesq@gmail.com>
2017-12-04 19:33:09 -0200

    Geração de Plataformas e aleatorização da posição X

--- Assets/Scripts/TouchesPlatform.cs
+++ Assets/Scripts/TouchesPlatform.cs
@@ -8,22 +8,29 @@ public class TouchesPlatform : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D target){
 		
+		Debug.Log (target.otherCollider.gameObject.transform.position.y);
 		if(!touched){
 			var platforms = GameObject.FindGameObjectsWithTag ("Platform");
+
 			foreach(var p in platforms){
-				if(p == target.gameObject)
+				if(p.gameObject.GetInstanceID() == target.otherCollider.gameObject.GetInstanceID())
 					continue;
-				Debug.Log("Aqui");
-				Debug.Log(p.transform.position.x);
-				if(p.transform.position.y < target.gameObject.transform.position.y){
-					var newY = target.gameObject.transform.position.y + 6.0f;
-					target.gameObject.transform.position = new Vector3 (target.gameObject.transform.position.x, newY, 0.0f);
+
+				Debug.Log ("target.otherCollider.gameObject.transform.position.y: " + target.otherCollider.gameObject.transform.position.y);
+				Debug.Log ("p.transform.position.y: " + p.transform.position.y);
+
+				if(p.transform.position.y < target.otherCollider.gameObject.transform.position.y){
+					var newX = Random.Range (1000, 2258) / 1000.0f;
+					var newY = p.transform.position.y + 4.5f;
+					p.transform.position = new Vector3 (newX, newY, 0.0f);
+					p.GetComponent<TouchesPlatform> ().touched = false;
 				}
 			}
+
+			// Contador
 		}
 		
 		touched = true;
-		
 	}
 
 }
