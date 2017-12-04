8cd4c5ed3c51a6b9f9e549b7278cebf10cafc42a
Braulio Mesquita <brauliomesq@gmail.com>
2017-12-03 23:17:33 -0200

    Initial Commit - Jumping Character

---
+++ Assets/Scripts/CameraFollower2D.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower2D : MonoBehaviour {
+
+	private Transform target;
+
+	public float boundsBottom = 0.0f;
+
+	public Vector2 movementFromBase = new Vector2 (0.0f, 0.0f);
+	public Vector2 baseLocation = new Vector2(0.0f, 0.0f);
+
+	void Start () {
+		target = GameObject.FindWithTag ("Player").transform;
+
+		baseLocation = new Vector2 (transform.position.x, transform.position.y);
+	}
+	
+	void Update () {
+		transform.position = new Vector3 (transform.position.x, Mathf.Max(target.position.y, boundsBottom), transform.position.z);
+		movementFromBase = new Vector2 (transform.position.x - baseLocation.x, transform.position.y - baseLocation.y);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private Rigidbody2D _rb;
+	private Animator _an;
+
+	public float speed = 10f;
+	public float jumpForce = 400f;
+	public float maxVelocityX = 4f;
+
+	private bool _grounded;
+
+	void Start () {
+		_rb = GetComponent<Rigidbody2D> ();	
+		_an = GetComponent<Animator> ();	
+	}
+	
+	void FixedUpdate () {
+		var force = new Vector2 (0f, 0f);
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+		var absVelX = Mathf.Abs (_rb.velocity.x);
+		var absVelY = Mathf.Abs (_rb.velocity.y);
+
+		_grounded = absVelY == 0;
+
+		if (absVelX < maxVelocityX) {
+			force.x = speed * moveHorizontal;
+		}
+
+		if (_grounded && Input.GetButton ("Jump")) {
+			_grounded = false;
+			force.y = jumpForce;
+			_an.SetInteger ("AnimState", 1);
+		}
+
+		_rb.AddForce (force);
+
+		if (moveHorizontal > 0) {
+			transform.localScale = new Vector3 (1, 1, 1);
+			if (_grounded) {
+				_an.SetInteger ("AnimState", 2);
+			}
+		} else if (moveHorizontal < 0) {
+			transform.localScale = new Vector3 (-1, 1, 1);
+			if (_grounded) {
+				_an.SetInteger ("AnimState", 2);
+			}
+		} else if (_grounded){
+			_an.SetInteger ("AnimState", 0);
+		}
+
+	}
+}
